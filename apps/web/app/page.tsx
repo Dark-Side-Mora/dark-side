8405be52f83a58079506af9fa13edb47a947ca54
Sithum Bimsara <sithum.22@cse.mrt.ac.lk>
2025-12-27 08:09:07 +0530

    test: testing 2 lint-staged configuration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,8 +13,6 @@ const ThemeImage = (props: Props) => {
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
 
-      <Image {...rest} src={srcLight} className="imgLight" />
-
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
   );
